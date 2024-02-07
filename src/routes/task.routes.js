cee9f91ed8d2e04e6b66702f6b5b35471026102b
Jhostym <you@example.com>
2024-02-07 18:06:32 -0500

    iniciando backend

---
+++ src/routes/task.routes.js
@@ -0,0 +1,12 @@
+import {Router} from 'express';
+import { authRequired } from '../middlewares/validateToken.js';
+
+const router = Router();
+
+router.get('/tasks', authRequired,)
+router.get('/tasks/:id', authRequired,)
+router.get('/tasks', authRequired,)
+router.get('/tasks', authRequired,)
+router.get('/tasks', authRequired,)
+
+export default router;
