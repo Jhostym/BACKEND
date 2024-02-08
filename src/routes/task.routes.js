7e2003e76a56914db12d415b78bcfb9cffaad45c
Jhostym <you@example.com>
2024-02-08 12:52:43 -0500

    teminando backend por ahora

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,12 +1,15 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
+import { getTasks, getTask, createTasks, deleteTasks, updateTasks } from '../controllers/task.controller.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { createTasksSchema } from '../schemas/task.schema.js';
 
 const router = Router();
 
-router.get('/tasks', authRequired,)
-router.get('/tasks/:id', authRequired,)
-router.get('/tasks', authRequired,)
-router.get('/tasks', authRequired,)
-router.get('/tasks', authRequired,)
+router.get('/tasks', authRequired, getTasks)
+router.get('/tasks/:id', authRequired, getTask)
+router.post('/tasks', authRequired, validateSchema(createTasksSchema), createTasks)
+router.delete('/tasks/:id', authRequired, deleteTasks)
+router.put('/tasks/:id', authRequired, updateTasks)
 
 export default router;
