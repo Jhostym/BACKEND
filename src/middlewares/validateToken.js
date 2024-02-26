288a9cd141ee49238b9732912787d7e396e4c833
Jhostym <you@example.com>
2024-02-26 12:29:37 -0500

    hi

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,6 +4,8 @@ import { TOKEN_SECRET } from '../config.js';
 export const authRequired = (req, res, next) => {
   const { token } = req.cookies;
 
+  console.log(token)
+
   if (!token)
     return res.status(401).json({ message: 'Unauthorized' })
 
