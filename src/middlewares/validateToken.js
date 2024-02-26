1e049aa791a3f0a5871ec8b39a0dd4b6f6a005b8
Jhostym <you@example.com>
2024-02-26 12:44:50 -0500

    solu

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,17 +2,27 @@ import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../config.js';
 
 export const authRequired = (req, res, next) => {
-  const { token } = req.cookies;
+  try {
+    const token = req.cookies.token;
 
-  console.log(token)
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
 
-  if (!token)
-    return res.status(401).json({ message: 'Unauthorized' })
+    jwt.verify(token, TOKEN_SECRET, (err, decodedToken) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid token' });
+      }
 
-  jwt.verify(token, TOKEN_SECRET, (err, user) => {
-    if (err)
-      return res.status(403).json({ message: 'Invalid token' });
-    req.user = user;
-    next();
-  });
+      // Agrega el usuario decodificado al objeto de solicitud (req)
+      req.user = decodedToken;
+
+      // Continúa con el siguiente middleware
+      next();
+    });
+  } catch (error) {
+    // Manejo de errores internos del servidor
+    console.error('Error in authRequired middleware:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
